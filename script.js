411750c7d7f00124f46e29e6101027c27e1202a2
Ujjwal14102004 <ujjwal.k.s1410@gmail.com>
2025-03-10 21:54:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Dark/Light Mode Toggle
+document.getElementById("theme-toggle").addEventListener("click", function () {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+});
